87dd73163fd67583c810274b25db96d487791361
Jonathan <ouellettejonathan87@gmail.com>
2024-10-07 14:18:52 +0000

    add 2 verse

---
+++ mypoem.py
@@ -0,0 +1,18 @@
+print("""
+
+(verse 1) Whose girl is that? I think I know.
+Its owner is quite happy though.
+Full of joy like a vivid rainbow,
+I watch him laugh. I cry hello.
+
+(verse 2) He gives his girl a shake,
+And laughs until her belly aches.
+The only other sound's the break,
+Of distant waves and birds awake.
+
+(chours) The girl is awesome, enjoyable and deep,
+But he has promises to keep,
+After cake and lots of sleep.
+Sweet dreams come to him cheap.
+
+  """)
